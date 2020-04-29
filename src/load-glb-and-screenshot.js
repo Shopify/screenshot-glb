f0f4ada4f60b95594059bea2c3c27e5f9c075cbc
Gurpreet Gill <gurpreet.gill@shopify.com>
2020-04-29 14:21:37 -0400

    improve/fix logging when taking a screenshot

--- src/load-glb-and-screenshot.js
+++ src/load-glb-and-screenshot.js
@@ -1,20 +1,23 @@
 module.exports = async (page, {glbPath, outputPath, format, quality, timeout}) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     page.exposeFunction('resolvePromise', resolve);
     page.exposeFunction('rejectPromise', reject);
-    page.evaluate(async (browser_glbPath, browser_outputPath, browser_format, browser_quality, timeout) => {
-      var endTime = Number(new Date()) + timeout;
+    await page.evaluate(async (browser_glbPath, browser_outputPath, browser_format, browser_quality, timeout) => {
+      var startTime = Number(new Date());
+      var endTime = startTime + timeout;
 
       const isTimedOut = function() {
-        const currentTime = Number(new Date())
+        const currentTime = Number(new Date());
         if (currentTime < endTime) {
-          window.logInfo(`Waiting ${endTime - currentTime}ms for model to render...`);
-        }else{
+          window.logInfo(`--- Waited ${currentTime - startTime}ms for model to render. ${endTime - currentTime}ms left until timeout.`);
+        } else {
           window.rejectPromise('Waited until timeout');
         }
       }
 
       modelViewer = document.getElementById('snapshot-viewer');
+      timeoutSet = setInterval(isTimedOut, 1000);
+
       modelViewer.addEventListener('model-visibility', function(){
         const visible = event.detail.visible;
         if(visible){
@@ -22,9 +25,9 @@ module.exports = async (page, {glbPath, outputPath, format, quality, timeout}) =
             modelViewer.toDataURL(browser_format, browser_quality),
             browser_outputPath,
           );
+          clearTimeout(timeoutSet);
           window.resolvePromise();
         }
-        setInterval(isTimedOut, 1000);
       });
       modelViewer.src = browser_glbPath;
     }, glbPath, outputPath, format, quality, timeout);
