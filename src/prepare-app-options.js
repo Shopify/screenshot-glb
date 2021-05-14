0c5b2ba77d3dd6ee617ea68bd62e7bfe12e1adbb
Stephan Leroux <stephan.leroux@shopify.com>
2021-05-14 15:19:17 -0400

    Refactored/cleaned up screenshot logic borrowed from model-viewers fidelity test suite to avoid race conditions

--- src/prepare-app-options.js
+++ src/prepare-app-options.js
@@ -3,7 +3,7 @@ const path = require('path');
 const scrubOutput = require('./scrub-output');
 const colors = require('./colors.js');
 
-module.exports = function prepareBrowserOptions({libPort, modelPort, debug, argv}) {
+module.exports = function prepareBrowserOptions({libPort, modelPort, argv}) {
   const inputPath = `http://localhost:${modelPort}/${path.basename(argv.input)}`;
   const [outputPath, format] = scrubOutput(argv.output, argv.image_format);
   const defaultBackgroundColor = (format === 'image/jpeg' ? colors.white : colors.transparent);
@@ -14,10 +14,10 @@ module.exports = function prepareBrowserOptions({libPort, modelPort, debug, argv
     timeout: argv.timeout || 10000,
     height: argv.height || 1024,
     width: argv.width || 1024,
+    debug: argv.debug || false,
     inputPath,
     outputPath,
     format,
     libPort,
-    debug,
   };
 }
