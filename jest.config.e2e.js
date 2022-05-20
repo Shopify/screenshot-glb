e61af0b38e38e2eee370fe703088dfdb9f5b84d0
Mikko Haapoja <mikkoh@users.noreply.github.com>
2022-05-20 12:09:22 -0400

    Revert "Integration tests"

--- jest.config.e2e.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "./dist"],
-  testMatch: ["**/__tests__/**/*.e2e.[jt]s?(x)", "**/?(*.)+(spec|test).e2e.[jt]s?(x)"]
-};
